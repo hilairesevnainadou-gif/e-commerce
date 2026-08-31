f6cd6fc5698392c0587481cd396ea3a8d990dd89
hilairesevnainadou-gif <ainadougbelihanhilairehaymarce@gmail.com>
2026-08-31 23:02:55 +0100

    fix: mise à jour du port pour le serveur local de 8000 à 8002 dans la configuration Next.js

--- bloomtpl-1.0.0/next.config.ts
+++ bloomtpl-1.0.0/next.config.ts
@@ -19,7 +19,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
-        port: "8000",
+        port: "8002",
         pathname: "/**",
       },
     ],
